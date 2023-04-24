60e696da7a3e1ec84690e34cfda4d964738375fa
Noah Rahm <correctsyntax@yahoo.com>
2023-04-24 15:00:20 -0500

    Update base path

--- docs/_app/immutable/chunks/1.39663a7f.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/error.svelte.7e4be414.js";export{t as component};
--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,8 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
+const dev = process.argv.includes('dev');
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
@@ -15,7 +17,10 @@ const config = {
             fallback: null,
             precompress: false,
             strict: true
-        })
+        }),
+        paths: {
+            base: dev ? '' : process.env.BASE_PATH,
+        }
 	},
  	preprocess: vitePreprocess()
 };
